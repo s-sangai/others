ab4fe4440bb3b0fdff1ce4c06819fd43469dcf62
FuruyaSatoshi <s-sangai@users.noreply.github.com>
2019-01-26 22:27:10 +0900

    4096パターン全てアクセスしてcsv化させることはできた
    あとは質問内容をクローリングするようにしたいが、なぜかxpathでtext()を指定すると取得できない。

--- crawling_16personalities/crawling.py
+++ crawling_16personalities/crawling.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import time
 from selenium import webdriver
@@ -8,102 +9,138 @@ import chromedriver_binary
 # options = Options()
 # options.add_argument('--headless')
 # browser = webdriver.Chrome(chrome_options = options)
-browser = webdriver.Chrome()
 
 start_url = "https://www.motivation-up.com/whats/16type.html"
 
-def scraping():
-    browser.get(start_url)
-    time.sleep(1)
-    btn1 = browser.find_element_by_xpath('//*[@id="form1"]/p[1]/label[1]/input')
-    btn2 = browser.find_element_by_xpath('//*[@id="form1"]/p[2]/label[1]/input')
-    btn3 = browser.find_element_by_xpath('//*[@id="form1"]/p[3]/label[1]/input')
-    btn4 = browser.find_element_by_xpath('//*[@id="form1"]/p[4]/label[1]/input')
-    btn5 = browser.find_element_by_xpath('//*[@id="form1"]/p[5]/label[1]/input')
-    btn6 = browser.find_element_by_xpath('//*[@id="form1"]/p[6]/label[1]/input')
-    btn7 = browser.find_element_by_xpath('//*[@id="form1"]/p[7]/label[1]/input')
-    btn8 = browser.find_element_by_xpath('//*[@id="form1"]/p[8]/label[1]/input')
-    btn9 = browser.find_element_by_xpath('//*[@id="form1"]/p[9]/label[1]/input')
-    btn10 = browser.find_element_by_xpath('//*[@id="form1"]/p[10]/label[1]/input')
-    btn11 = browser.find_element_by_xpath('//*[@id="form1"]/p[11]/label[1]/input')
-    btn12 = browser.find_element_by_xpath('//*[@id="form1"]/p[12]/label[1]/input')
-    btn1.click()
-    btn2.click()
-    btn3.click()
-    btn4.click()
-    btn5.click()
-    btn6.click()
-    btn7.click()
-    btn8.click()
-    btn9.click()
-    btn10.click()
-    btn11.click()
-    btn12.click()
-
-    browser.find_element_by_class_name('sbt_1').click()
+def scrape_4096patterns():
+    options1 = range(1, 3)
+    options2 = range(1, 3)
+    options3 = range(1, 3)
+    options4 = range(1, 3)
+    options5 = range(1, 3)
+    options6 = range(1, 3)
+    options7 = range(1, 3)
+    options8 = range(1, 3)
+    options9 = range(1, 3)
+    options10 = range(1, 3)
+    options11 = range(1, 3)
+    options12 = range(1, 3)
+    combinations = [(option1, option2, option3, option4, option5, option6, option7, option8, option9, option10, option11, option12)
+    for option1 in options1
+    for option2 in options2
+    for option3 in options3
+    for option4 in options4
+    for option5 in options5
+    for option6 in options6
+    for option7 in options7
+    for option8 in options8
+    for option9 in options9
+    for option10 in options10
+    for option11 in options11
+    for option12 in options12]
     
-    type_name = browser.find_element_by_xpath('//*[@id="main"]/h2[4]').text
-    print(type_name)
-    print(type_name[0:4])
+    num = 1
+    df = pd.DataFrame(columns=['type', 'Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Q7', 'Q8', 'Q9', 'Q10', 'Q11', 'Q12'])
 
-
-
-def scrapin():
-    # initialization
-    test = ["1"]
-    # 大分類
-    large_classification_tag = []
-    large_classification = []
-    # 中分類
-    middle_url = []
-    middle_classification_tag = []
-    middle_classification = []
-    # 小分類
-    small_url = []
-    small_classification_tag = []
-    small_classification = []
-    small_classification_url = []
-
-    df = pd.DataFrame()
-
-    html = requests.get(start_url)
-    soup = BeautifulSoup(html.text, "html.parser")
-    elements = soup.find_all("ul", class_ = "listB01")
-    # print(rows)
-    # get urls
-    for large_classification_tag in elements:
-        for large_a in large_classification_tag.find_all("a"):
-            # print(large_a)
-            middle_url.append(base_url + large_a.attrs["href"])
-    # print("----------------------")
-    # 中分類
-    for middle_count in range(len(large_classification_tag)):
+    browser = webdriver.Chrome()
+    # 2^12パターンアクセスする
+    for option1, option2, option3, option4, option5, option6, option7, option8, option9, option10, option11, option12 in combinations:
+        browser.get(start_url)
         time.sleep(1)
-        middle_html = requests.get(middle_url[middle_count])
-        soup = BeautifulSoup(middle_html.text, "html.parser")
-        middle_elements = soup.find_all("ul", class_= "listB01")
-        for middle_classification_tag in middle_elements:
-            for middle_a in middle_classification_tag.find_all("a"):
-                small_url.append(base_url + middle_a.attrs["href"])
-        # print(small_url)
-        # 小分類
-        for small_count in range(2):
-            time.sleep(1)
-            small_html = requests.get(small_url[small_count])
-            soup = BeautifulSoup(small_html.text, "html.parser")
-            small_elements = soup.find_all("ul", class_= "listB01")
-    
-            for small_classification_tag in small_elements:
-                for small_a in small_classification_tag.find_all("a"):
-                    large_classification.append(soup.find("h1").string)
-                    middle_classification.append(soup.find("p", class_="headingI01").string)
-                    small_classification.append(small_a.string)
-                    small_classification_url.append(base_url + small_a.attrs["href"])
+        element1 = '//*[@id="form1"]/p[1]/label[' + str(option1) + ']/input'
+        element2 = '//*[@id="form1"]/p[2]/label[' + str(option2) + ']/input'
+        element3 = '//*[@id="form1"]/p[3]/label[' + str(option3) + ']/input'
+        element4 = '//*[@id="form1"]/p[4]/label[' + str(option4) + ']/input'
+        element5 = '//*[@id="form1"]/p[5]/label[' + str(option5) + ']/input'
+        element6 = '//*[@id="form1"]/p[6]/label[' + str(option6) + ']/input'
+        element7 = '//*[@id="form1"]/p[7]/label[' + str(option7) + ']/input'
+        element8 = '//*[@id="form1"]/p[8]/label[' + str(option8) + ']/input'
+        element9 = '//*[@id="form1"]/p[9]/label[' + str(option9) + ']/input'
+        element10 = '//*[@id="form1"]/p[10]/label[' + str(option10) + ']/input'
+        element11 = '//*[@id="form1"]/p[11]/label[' + str(option11) + ']/input'
+        element12 = '//*[@id="form1"]/p[12]/label[' + str(option12) + ']/input'
+        btn1 = browser.find_element_by_xpath(element1)
+        btn2 = browser.find_element_by_xpath(element2)
+        btn3 = browser.find_element_by_xpath(element3)
+        btn4 = browser.find_element_by_xpath(element4)
+        btn5 = browser.find_element_by_xpath(element5)
+        btn6 = browser.find_element_by_xpath(element6)
+        btn7 = browser.find_element_by_xpath(element7)
+        btn8 = browser.find_element_by_xpath(element8)
+        btn9 = browser.find_element_by_xpath(element9)
+        btn10 = browser.find_element_by_xpath(element10)
+        btn11 = browser.find_element_by_xpath(element11)
+        btn12 = browser.find_element_by_xpath(element12)
+        btn1.click()
+        btn2.click()
+        btn3.click()
+        btn4.click()
+        btn5.click()
+        btn6.click()
+        btn7.click()
+        btn8.click()
+        btn9.click()
+        btn10.click()
+        btn11.click()
+        btn12.click()
 
-    print(large_classification)
-    print(middle_classification)
-    print(small_classification)
-    print(small_classification_url)
+        browser.find_element_by_class_name('sbt_1').click()
+        type_name = browser.find_element_by_xpath('//*[@id="main"]/h2[4]').text
+        
+        df.loc[num] = [type_name[0:4], str(option1), str(option2), str(option3), str(option4), str(option5), str(option6), str(option7), str(option8), str(option9), str(option10), str(option11), str(option12)]
+        
+        num = num + 1
+        
+        # For Debug 
+        # if option10==2:
+        #     break
+
+    df.to_csv('16personalities.csv', index=False, mode='w', encoding='utf-8')
+    browser.quit()
+
+def scrape_answers():
+    browser = webdriver.Chrome()
+    browser.get(start_url)
+    
+    q1a1 = browser.find_element_by_xpath('//*[@id="form1"]/p[1]/label[1]')
+    q2a1 = browser.find_element_by_xpath('//*[@id="form1"]/p[2]/label[1]')
+    q3a1 = browser.find_element_by_xpath('//*[@id="form1"]/p[3]/label[1]')
+    q4a1 = browser.find_element_by_xpath('//*[@id="form1"]/p[4]/label[1]')
+    q5a1 = browser.find_element_by_xpath('//*[@id="form1"]/p[5]/label[1]')
+    q6a1 = browser.find_element_by_xpath('//*[@id="form1"]/p[6]/label[1]')
+    q7a1 = browser.find_element_by_xpath('//*[@id="form1"]/p[7]/label[1]')
+    q8a1 = browser.find_element_by_xpath('//*[@id="form1"]/p[8]/label[1]')
+    q9a1 = browser.find_element_by_xpath('//*[@id="form1"]/p[9]/label[1]')
+    q10a1 = browser.find_element_by_xpath('//*[@id="form1"]/p[10]/label[1]')
+    q11a1 = browser.find_element_by_xpath('//*[@id="form1"]/p[11]/label[1]')
+    q12a1 = browser.find_element_by_xpath('//*[@id="form1"]/p[12]/label[1]')
+
+    q1a2 = browser.find_element_by_xpath('//*[@id="form1"]/p[1]/label[2]')
+    q2a2 = browser.find_element_by_xpath('//*[@id="form1"]/p[2]/label[2]')
+    q3a2 = browser.find_element_by_xpath('//*[@id="form1"]/p[3]/label[2]')
+    q4a2 = browser.find_element_by_xpath('//*[@id="form1"]/p[4]/label[2]')
+    q5a2 = browser.find_element_by_xpath('//*[@id="form1"]/p[5]/label[2]')
+    q6a2 = browser.find_element_by_xpath('//*[@id="form1"]/p[6]/label[2]')
+    q7a2 = browser.find_element_by_xpath('//*[@id="form1"]/p[7]/label[2]')
+    q8a2 = browser.find_element_by_xpath('//*[@id="form1"]/p[8]/label[2]')
+    q9a2 = browser.find_element_by_xpath('//*[@id="form1"]/p[9]/label[2]')
+    q10a2 = browser.find_element_by_xpath('//*[@id="form1"]/p[10]/label[2]')
+    q11a2 = browser.find_element_by_xpath('//*[@id="form1"]/p[11]/label[2]')
+    q12a2 = browser.find_element_by_xpath('//*[@id="form1"]/p[12]/label[2]')
+    
+    df = pd.DataFrame(columns=['Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Q7', 'Q8', 'Q9', 'Q10', 'Q11', 'Q12'])
+    df1 = pd.Series([str(q1a1), str(q2a1), str(q3a1) ,str(q4a1) ,str(q5a1) ,str(q6a1) ,str(q7a1) ,str(q8a1) ,str(q9a1) ,str(q10a1) ,str(q11a1) ,str(q12a1)], index=df.columns)
+    df2 = pd.Series([str(q1a2), str(q2a2), str(q3a2) ,str(q4a2) ,str(q5a2) ,str(q6a2) ,str(q7a2) ,str(q8a2) ,str(q9a2) ,str(q10a2) ,str(q11a2) ,str(q12a2)], index=df.columns)
+    
+    df.append(df1, ignore_index=True)
+    df.append(df2, ignore_index=True)
+    
+    df.to_csv('personalities_Questions_list.csv', index=False, mode='w', encoding='utf-8')
+    browser.quit()
 
 if __name__ == "__main__":
-    scraping()
+    scrape_4096patterns()
+    time.sleep(1)
+    # print("------DEBUG-------")
+    # scrape_answers()
+    print("------FINISH------")
